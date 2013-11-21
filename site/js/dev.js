9eae30015dfae8bba2fc7ebce2f5af922b83fce6
Tomi Mickelsson <atomi@iki.fi>
2013-11-21 19:18:25 +0200

    Initial site kuntakartta.org

---
+++ site/js/dev.js
@@ -0,0 +1,112 @@
+
+// kuntakartta.org dev samples
+
+(function($) {
+
+    $(document).ready(function() {
+        main();
+    });
+
+    function main() {
+        case1();
+        case2();
+        case3();
+    }
+
+    function case1() {
+
+        var options = {url_geoson: "data/kuntarajat.geojson"};
+        var map = $("#map1").mapcolorizer(options).data("mapcolorizer");
+        map.init(function() {
+            map.loadData("data/2014-tulovero.json");
+            map.setTileServer("mapquest");
+        });
+    }
+
+    function case2() {
+        // some data
+        var data = {area049:50, area091: 12, area638: 110,
+            area224: 35, area710: 100, area505: 70};
+
+        // color scale
+        var colors = ['#ffffe0', '#ffe2bd', '#ffc39c', '#ffa480',
+            '#ff7f6a', '#f9575c', '#e63652', '#cb1942', '#ac0528', '#8b0000'];
+
+        var options = {url_geoson: "data/kuntarajat-uusimaa.geojson",
+            color_scale: colors};
+        var map = $("#map2").mapcolorizer(options).data("mapcolorizer");
+        map.init(function() {
+            map.showNames(true, true);
+            map.loadData(data);
+        });
+    }
+
+    function case3() {
+        var active_opt = "1";
+
+        // show only these areas of whole uusimaa
+        var arealist = [
+            "area927", // vihti
+            "area257", // knummi
+            "area049", // espoo
+            "area235", // kauniainen
+            "area091", // hki
+            "area092", // vantaa
+            "area753", // sipoo
+            "area106", // hyvinkaa
+            "area543", // nurmijarvi
+            "area858", // tuusula
+            "area245", // kerava
+            "area186", // jarvenpaa
+            "area505", // mantsala
+            "area611"  // pornainen
+        ];
+
+        // specify colors for each option
+        var colors = {
+            "1": {area927:0, area257:0, area049:0, area235:0, area091:1,
+                  area092:2, area753:3, area106:3, area543:3, area858:3,
+                  area245:3, area186:3, area505:3, area611:3},
+            "2": {area927:0, area257:1, area049:1, area235:1, area091:1,
+                  area092:1, area753:1, area106:0, area543:0, area858:0,
+                  area245:0, area186:0, area505:0, area611:0},
+            "3": {area927:0, area257:0, area049:0, area235:0, area091:0,
+                  area092:0, area753:0, area106:0, area543:0, area858:0,
+                  area245:0, area186:0, area505:0, area611:0}
+        };
+
+        var cols = ["#aaf", "yellow", "#faa", "#afa"];
+
+        // return color based on 'opt'
+        function style(area) {
+            var code = area.properties.code;
+            var colorindex = colors[active_opt][code];
+            var color = cols[colorindex];
+            return {
+                color: "#333",
+                weight: 2,
+                opacity: 1.0,
+                fillOpacity: 1.0,
+                fillColor: color
+            };
+        }
+
+        // init map
+        var options = {url_geoson: "data/kuntarajat-uusimaa.geojson",
+            style_func: style, "area_list":arealist,
+            min_zoom: 8};
+        var map = $("#map3").mapcolorizer(options).data("mapcolorizer");
+        map.init(function() {
+            map.showNames(true, true);
+        });
+
+        // change option
+        $("a[data-opt]").click(function(){
+            active_opt = $(this).attr("data-opt");
+            map.setStyle(style); // set new styles
+            return false;
+        });
+    }
+
+})(jQuery);
+
---
+++ site/js/main.js
@@ -0,0 +1,107 @@
+
+// kuntakartta.org main
+
+(function($) {
+
+    var map;
+
+    $(document).ready(function() {
+        main();
+    });
+
+    function main() {
+        console.debug("main");
+
+        set_map_height();
+
+        // change dataset
+        $("#dataset").change(function() {
+            var sel = $(this).val();
+            console.debug("change " + sel);
+
+            if (window.location.hash != sel)
+                window.location = sel;
+        });
+
+        // change background image/tiles
+        $("#overlaytype").change(function() {
+            var sel = $(this).val();
+
+            if (sel == "img") {
+                var url = "img/Finland-yleiskartta.jpg";
+                var bounds = [[70.9,17.1],[59.34, 33.05]];
+                map.setImage(url, bounds);
+            } else {
+                map.setTileServer(sel);
+            }
+        });
+
+        // show/hide names
+        $("#names").click(function() {
+            var sel = $(this).prop("checked");
+            map.showNames(sel, true);
+        });
+
+        // display error
+        $(document).ajaxError(function() {
+            $("#errtxt").show();
+        });
+
+        // hash in url changed
+        $(window).on('hashchange', function () {
+            console.debug("hashchange");
+            load_data();
+        });
+
+        var options = {url_geoson: "data/kuntarajat.geojson"};
+        map = $("#map").mapcolorizer(options).data("mapcolorizer");
+        map.init(function() {
+
+            $("#overlaytype option[value=mapquest]").prop('selected', true);
+            $("#overlaytype").change();
+
+            map.refresh();
+
+            load_data();
+        });
+    }
+
+    // load json data depending on select#dataset value
+    function load_data() {
+        var h = window.location.hash;
+        if (!h)
+            h = "#2014-tulovero";
+
+        $("#dataset option[value='"+h+"']").prop('selected', true);
+
+        var fname = h.slice(1);
+        if (!fname)
+            fname = null;
+
+        $("#errtxt").hide();
+
+        if (fname == "none")
+            fname = null;
+        else
+            fname = "data/" + fname + ".json";
+
+        map.loadData(fname, function(bounds){
+            $("#dmin").text(bounds.min);
+            $("#dmax").text(bounds.max);
+            $("#davg").text(bounds.avg);
+
+            // remove spinner
+            $("body").addClass("ready");
+        });
+    }
+
+    // adjusts the height of map, extending almost to page bottom
+    function set_map_height() {
+        var h = $(window).height() - $("#map").offset().top - 70;
+        if (h < 300)
+            h = 300;
+        $("#map").height(h);
+    }
+
+})(jQuery);
+
